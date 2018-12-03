a0198882119af1395cb6b8ed7c6c0479309f61d7
Alex Voet <advoet@gmail.com>
2018-12-02 19:56:39 -0800

    wrote skeleton for graph generator

---
+++ generator.py
@@ -0,0 +1,15 @@
+"""Script to generate graphs with given chromatic number"""
+
+from graph_tool.all import *
+
+test_one = (5, 11, 150, 5, 8401, 6859, 84035, (19,60,97,210))
+
+def main():
+    pass
+
+def generate_test_graph(chi, d, n, k, a, c, m, b_vec):
+    g = Graph(directed=False)
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())

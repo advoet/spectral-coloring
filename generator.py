5cf1dba6eec0ac783e9dae45e875122e8a113d63
Alex Voet <advoet@gmail.com>
2019-05-21 10:48:39 -0700

    A few comments, no structure change

--- generator.py
+++ generator.py
@@ -7,6 +7,9 @@ import numpy
 import numpy.linalg as linalg
 import time
 
+
+# Tests from Leighton Appendix C
+# (Number of nodes, chromatic number, a, c, m, (b))
 tests = ((150, 5, 8401, 6859, 84035, (19,60,97,210)),
          (150, 5, 8401, 6859, 84035, (39,120,195,420)),
          (150, 5, 8401, 6859, 84035, (58,180,292,630)),
@@ -27,6 +30,7 @@ def main():
     g2 = generate_strict_three_colorable(5, .5)
     graph_draw(g2, vertex_text = g2.vertex_index, output="three.png", fmt="auto")
 
+    #Unwrap test cases and generate image of test graph
     g = generate_test_graph(*tests[-1])
     graph_draw(g, vertex_text = g.vertex_index, vertex_font_size=10,
                 output="test.png", fmt = "auto")
@@ -35,14 +39,12 @@ def generate_strict_three_colorable(k, p):
     """Generates a graph with 3k nodes of chromatic number 3"""
     g = generate_n_colorable(3, k, p)
     while is_bipartite(g):
+        #If g happens to be 2 colorable, try again.
         g = generate_n_colorable(3, k, p)
     return g
 
 def generate_three_colorable(k, p):
-    """Generates a three colorable graph with 3k nodes
-    
-    Not used anymore, replaced by generate_n_colorable with n=3
-    """
+    """Generates a three colorable graph with 3k nodes"""
     g = Graph(directed=False)
     g.add_vertex(3*k)
     for vs in chain(product(range(0,k),range(k,2*k)), product(range(0,k),range(2*k,3*k)), product(range(k,2*k),range(2*k,3*k))):
@@ -58,6 +60,7 @@ def generate_strict_n_colorable(n, k, p):
     """Generates a graph with chromatic number n by introducing an n-clique"""
     g = generate_n_colorable(n, k, p)
     add_clique_2(n, g, [i*k for i in range(0,n)], 0)
+    return g
 
 def generate_n_colorable(n, k, p):
     """Generates an n colorable graph with n*k nodes, edges with probability p"""
@@ -67,11 +70,13 @@ def generate_n_colorable(n, k, p):
     #this line splits nodes into color groups, currently even split
     groups = [range(i*k,(i+1)*k) for i in range(0,n)]
     
+    #adds an edge to the list between nodes from different groups (colors) with
+    #probability p
     g.add_edge_list([pairs for pairs in chain(*[product(range_1,range_2) for range_1,range_2 in combinations(groups, 2)]) if random() < p])
     return g
     
 def generate_test_graph(n, k, a, c, m, bs):
-    """Leighton's algorithm for generating graph with chromatic number"""
+    """Leighton's algorithm for generating graph with given chromatic number k"""
     g = Graph(directed=False)
     g.add_vertex(n)
     
@@ -79,6 +84,7 @@ def generate_test_graph(n, k, a, c, m, bs):
     
     ys = [x % n for x in xs] #no longer uniform sequence
     
+    #See Leighton '79
     start_index = 0
     clique_size = len(bs)+1
     for b in bs:
@@ -111,6 +117,7 @@ def add_clique(clique_size, graph, ys, start_index):
                 graph.add_edge(graph.vertex(ys[(start_index + i) % len(ys)]),
                            graph.vertex(ys[(start_index + j) % len(ys)]))
 
+#Slightly optimized one line version of add_clique
 def add_clique_2(clique_size, graph, ys, start_index):
     """Adds a clique to a graph from a vector ys of vertex indices"""
     graph.add_edge_list([pairs for pairs in combinations([ys[i % len(ys)] for i in range(start_index, start_index + clique_size)],2) if not graph.edge(pairs[0], pairs[1])])
